cd821fd116d8e975030be4c7a68955ae5514bbd7
dave glynn <dave@ocl.ie>
2016-05-18 13:51:13 +0100

    initial checkin

--- app/app.component.js
+++ app/app.component.js
@@ -1,4 +1,4 @@
-System.register(['@angular/core'], function(exports_1, context_1) {
+System.register(['@angular/core', '@angular/router-deprecated', './navbar.component', './home.component', './users/users.component', './users/user-form.component', './posts/posts.component', './not-found.component'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,21 +10,52 @@ System.register(['@angular/core'], function(exports_1, context_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
+    var core_1, router_deprecated_1, navbar_component_1, home_component_1, users_component_1, user_form_component_1, posts_component_1, not_found_component_1;
     var AppComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (router_deprecated_1_1) {
+                router_deprecated_1 = router_deprecated_1_1;
+            },
+            function (navbar_component_1_1) {
+                navbar_component_1 = navbar_component_1_1;
+            },
+            function (home_component_1_1) {
+                home_component_1 = home_component_1_1;
+            },
+            function (users_component_1_1) {
+                users_component_1 = users_component_1_1;
+            },
+            function (user_form_component_1_1) {
+                user_form_component_1 = user_form_component_1_1;
+            },
+            function (posts_component_1_1) {
+                posts_component_1 = posts_component_1_1;
+            },
+            function (not_found_component_1_1) {
+                not_found_component_1 = not_found_component_1_1;
             }],
         execute: function() {
             AppComponent = (function () {
                 function AppComponent() {
                 }
                 AppComponent = __decorate([
+                    router_deprecated_1.RouteConfig([
+                        { path: '/', name: 'Home', component: home_component_1.HomeComponent },
+                        { path: '/users', name: 'Users', component: users_component_1.UsersComponent },
+                        { path: '/users/:id', name: 'EditUser', component: user_form_component_1.UserFormComponent },
+                        { path: '/users/new', name: 'NewUser', component: user_form_component_1.UserFormComponent },
+                        { path: '/posts', name: 'Posts', component: posts_component_1.PostsComponent },
+                        { path: '/not-found', name: 'NotFound', component: not_found_component_1.NotFoundComponent },
+                        { path: '/*other', name: 'Other', redirectTo: ['Home'] }
+                    ]),
                     core_1.Component({
                         selector: 'my-app',
-                        template: '<h1>My First Angular 2 App</h1>'
+                        template: "\n        <navbar></navbar>\n        <div class=\"container\">\n            <router-outlet></router-outlet>\n        </div>\n    ",
+                        directives: [navbar_component_1.NavBarComponent, router_deprecated_1.ROUTER_DIRECTIVES]
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
