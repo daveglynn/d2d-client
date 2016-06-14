ade57be67baf839340cc3af34078ccb077b2c57f
dave glynn <dave@ocl.ie>
2016-06-14 12:56:57 +0100

    Before start of fixing user

---
+++ app/shared/common.service.ts
@@ -0,0 +1,28 @@
+﻿'use strict'
+import {Injectable} from "@angular/core";
+import {URLSearchParams} from '@angular/http';
+import { LocalData } from '../shared/common';
+
+@Injectable()
+
+export class CommonService {
+
+    getToken( ) {
+        let params: URLSearchParams = new URLSearchParams();
+        params.set('Auth', localStorage.getItem('token'));
+        return params;
+    }
+
+    clearLocalStorage() {
+        localStorage.clear();
+    }
+
+    setLocalStorage(localData: LocalData) {
+        localStorage.setItem('token', localData.token);
+        localStorage.setItem('userId', localData.userId);
+        localStorage.setItem('firstName', localData.firstName);
+        localStorage.setItem('lastName', localData.lastName);
+        localStorage.setItem('email', localData.email);
+    }
+    
+}
