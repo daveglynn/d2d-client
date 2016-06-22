94b04fe4c9f86da692b2cda76768bf7d9f8ea31b
dave glynn <dave@ocl.ie>
2016-06-22 16:11:41 +0100

    error processing

--- app/errors/error.service.ts
+++ app/errors/error.service.ts
@@ -12,9 +12,12 @@ export class ErrorService {
                 errorData = new Error(error.status, "Authentication is required for this screen");
             }
         }  
-        if ((errorData.title === "") && (JSON.parse(error._body).message != 'undefined') && (JSON.parse(error._body).message != undefined)) {
-            errorData = new Error(JSON.parse(error._body).title, JSON.parse(error._body).message);
-        }  
+        if ((error._body != undefined) && (error._body != 'undefined')) {
+ 
+            if ((errorData.title === "") && (JSON.parse(error._body).message != 'undefined') && (JSON.parse(error._body).message != undefined)) {
+                errorData = new Error(JSON.parse(error._body).title, JSON.parse(error._body).message);
+            }  
+        }
         if ((errorData.title === "") && (error.message != 'undefined') && (error.message != undefined)) {
             errorData = new Error(error.title, error.message);
         }  
