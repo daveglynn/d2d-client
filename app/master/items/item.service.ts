b20ab9796fefce324c1c69182833f4d3958951d9
dave glynn <dave@ocl.ie>
2016-08-29 21:51:10 +0100

    Generation changes

--- app/master/items/item.service.ts
+++ app/master/items/item.service.ts
@@ -87,6 +87,17 @@ export class ItemService {
 
     }
 
-	
+    getItemsByListId(listId,filter?) {
+        var parms = {};
+        if (filter && filter.view) {
+            parms['view'] = filter.view;
+        }
+    
+        const headers = new Headers({ 'Content-Type': 'application/json' });
+        return this._http.get(this._url + "/item/list/" + listId, { search: this._commonService.setParms(parms) })
+
+            .map(res => res.json())
+            .catch(error => Observable.throw(error.json()))
+    }	
 	
 }
